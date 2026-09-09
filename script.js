e6b34dbd4fdf6466abe9a068ea84e29f03bb1b0e
Top Bomba AI <tapbomba.ai@gmail.com>
2026-09-09 20:56:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function order(productName, price) {
-    const phoneNumber = '2349016412919';
-    const message = `I would like to order ${productName} for N${price}.`;
-    const whatsappUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
-    window.open(whatsappUrl, '_blank');
+  const phoneNumber = '2349016412919';
+  const message = `I would like to order ${productName} for N${price}.`;
+  const whatsappUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
+  window.open(whatsappUrl, '_blank');
 }
